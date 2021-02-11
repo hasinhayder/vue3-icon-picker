a8ad1fd23d5d51cb3cc9ab5be35a31f789040a62
Hasin Hayder <countdraculla@gmail.com>
2021-02-12 01:54:33 +0600

    Vite Config for UMD and ESM Build

---
+++ src/entry.js
@@ -0,0 +1,3 @@
+import Vue3IconPicker from "./components/Vue3IconPicker.vue"
+
+export default Vue3IconPicker
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,39 @@
 import vue from '@vitejs/plugin-vue'
 
-/**
- * https://vitejs.dev/config/
- * @type {import('vite').UserConfig}
- */
 export default {
   plugins: [vue()],
   server: {
     open: '/index.html'
   }
 }
+
+
+/*
+//Use This COnfig For UMD and ESM Build
+import vue from "@vitejs/plugin-vue"
+const path = require("path")
+const replace = require("@rollup/plugin-replace")
+
+export default {
+  plugins: [
+    vue(),
+    replace({
+      "process.env.NODE_ENV": JSON.stringify("production"),
+    }),
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/entry.js"),
+      name: "Vue3IconPicker",
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+}
+*/
